822140ea2c39f4f2df6aa12ae5f0db37cb95a440
IshiharaRioto <21aj013@ms.dendai.ac.jp>
2023-06-02 14:32:22 +0900

    aaa

--- AnaogClock/index.js
+++ AnaogClock/index.js
@@ -1,22 +1,15 @@
 window.onload = function() {
-    const svg = document.getElementById('clock');
-
     function updateTime() {
         const now = new Date();
         const seconds = now.getSeconds();
         const minutes = now.getMinutes();
         const hours = (now.getHours() % 12) + minutes / 60;
 
-        const secondHand = svg.querySelector('.second-hand');
-        secondHand.setAttribute('transform', `rotate(${seconds * 6}, 50, 50)`);
-
-        const minuteHand = svg.querySelector('.minute-hand');
-        minuteHand.setAttribute('transform', `rotate(${minutes * 6}, 50, 50)`);
-
-        const hourHand = svg.querySelector('.hour-hand');
-        hourHand.setAttribute('transform', `rotate(${hours * 30}, 50, 50)`);
+        document.querySelector('.second-hand').style.transform = `rotate(${seconds * 6}deg)`;
+        document.querySelector('.minute-hand').style.transform = `rotate(${minutes * 6}deg)`;
+        document.querySelector('.hour-hand').style.transform = `rotate(${hours * 30}deg)`;
     }
 
     setInterval(updateTime, 1000);
+    updateTime();
 }
-
