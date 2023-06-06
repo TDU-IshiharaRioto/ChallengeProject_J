dd8934ccce644a453c6d3683c52e13ddc0f0ab7f
KST電子工作同好会_LED得点掲示板 <105205538+NaKaTsu1106@users.noreply.github.com>
2023-06-06 12:27:09 +0900

    サンプル画面追加

--- display/test/index.js
+++ display/test/index.js
@@ -3,10 +3,13 @@ window.onload = function() {
 
     function updateTime() {
         const now = new Date();
+        const year = now.getFullYear().toString().padStart(4, '0');
+        const month = (now.getMonth() + 1).toString().padStart(2, '0');
+        const day = now.getDate().toString().padStart(2, '0');
         const hours = now.getHours().toString().padStart(2, '0');
         const minutes = now.getMinutes().toString().padStart(2, '0');
         const seconds = now.getSeconds().toString().padStart(2, '0');
-        timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+        timeElement.textContent = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
     }
 
     setInterval(updateTime, 1000);
