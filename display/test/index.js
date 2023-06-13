b37edea03bad06dfe632abc2cb1fa1a6cb4ffe88
KST電子工作同好会_LED得点掲示板 <105205538+NaKaTsu1106@users.noreply.github.com>
2023-06-13 12:01:14 +0900

    Update index.js

--- display/test/index.js
+++ display/test/index.js
@@ -2,9 +2,10 @@ window.onload = function() {
     const timeElement = document.getElementById('time');
     const weatherElement = document.getElementById('weather');
     const yobi= new Array("日","月","火","水","木","金","土");
-    const json = `{ "date": "2023-6-13", "name": "AAA", "age": 20 }`;
+    const json = `{ "date": "2023-6-13","hour":"14", "weather": "晴れ", "maxtemp": "25","mintemp": "18" }`;
+    const parsed = JSON.parse(json);
 
-    weatherElement.textContent = '晴れ';
+    weatherElement.textContent = `[${parsed.weather}]  ${parsed.maxtemp}/${parsed.mintemp}℃`;
     
     function updateTime() {
         const now = new Date();
