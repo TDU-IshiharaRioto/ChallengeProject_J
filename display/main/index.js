4acc982c4872c6c903e9834249a2297454be583d
KST電子工作同好会_LED得点掲示板 <105205538+NaKaTsu1106@users.noreply.github.com>
2023-06-16 15:04:20 +0900

    Update index.js

--- display/main/index.js
+++ display/main/index.js
@@ -2,21 +2,16 @@ window.onload = function() {
     const timeElement = document.getElementById('time');
     const weatherElement = document.getElementById('weather');
     const yobi= new Array("日","月","火","水","木","金","土");
-    const json = `{ "date": "2023-6-13","hour":"14", "weather": "晴れ", "maxtemp": "25","mintemp": "18" }`;
-    const parsed = JSON.parse(json);
-
-    weatherElement.textContent = `[${parsed.weather}]  ${parsed.maxtemp}/${parsed.mintemp}℃`;
-    
     
     var req = new XMLHttpRequest();		  // XMLHttpRequest オブジェクトを生成する
     req.onreadystatechange = function() {		  // XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
         if(req.readyState == 4 && req.status == 200){ // サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
-        //alert(req.responseText);		          // 取得した JSON ファイルの中身を表示
+        alert(req.responseText);		          // 取得した JSON ファイルの中身を表示
         const parsed = JSON.parse(req.responseText);
         document.getElementById('weather_info').textContent = parsed.weather;
         }
     };
-    req.open("GET", "http://127.0.0.1/ChallengeProject_J/weather.json", false); // HTTPメソッドとアクセスするサーバーの　URL　を指定
+    req.open("GET", "http://127.0.0.1/ChallengeProject_J/weather.json", false); // HTTPメソッドとアクセスするサーバーのURLを指定
     req.send(null);					    // 実際にサーバーへリクエストを送信
 
 
