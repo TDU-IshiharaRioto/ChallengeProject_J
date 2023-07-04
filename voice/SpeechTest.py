6e034c32ee25a63dbc55e939fb366987a25c3e2b
MukaiKengo <134676522+MukaiKengo@users.noreply.github.com>
2023-07-04 12:29:34 +0900

    メモする機能を消した

--- voice/SpeechTest.py
+++ voice/SpeechTest.py
@@ -2,14 +2,14 @@
 
 import speech_recognition as sr
 from datetime import datetime
-
+"""
 #文字起こしファイルのファイル名を日付のtxtファイルとする
 filename = datetime.now().strftime('%Y%m%d_%H-%M-%S')
 txt =filename +".txt"
 
 with open(txt, 'w') as f: #txtファイルの新規作成 
     f.write(filename + "\n") #最初の一行目にはfilenameを記載する
-
+"""
 r = sr.Recognizer()
 mic = sr.Microphone()
 
@@ -28,8 +28,10 @@ while True:
         if r.recognize_google(audio, language='ja-JP') == "こんにちは" :#こんにちはを検出したらこんにちはを返す
             print("こんにちは画面をうつします")
             break
+        """
         with open(txt,'a') as f: #ファイルの末尾に追記していく
             f.write("\n" + r.recognize_google(audio, language='ja-JP'))
+        """
     #認識できなかった時に止まらないようにする
     except sr.UnknownValueError:
         print("録音できませんでした")
