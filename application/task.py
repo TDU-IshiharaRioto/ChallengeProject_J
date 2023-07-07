86a93bded72670123a284aa5ced73d529e7c9b61
takayu <134676218+takayu39@users.noreply.github.com>
2023-07-07 15:19:42 +0900

    timetree apiのテスト

--- application/task.py
+++ application/task.py
@@ -1,22 +1,28 @@
 import requests
 import json
-import token
+import mytoken
 
-ACCESS_TOKEN = token.timetree_token
-CALENDAR_ID = 'カレンダーのID'
+# 現在はトークンで認証しているが、いずれOAuth認証にする
+ACCESS_TOKEN = mytoken.timetree_token
+CALENDAR_ID = mytoken.timetree_calendar_id
+
+def get_caleder():
+    return
 
 # ヘッダーの設定
-headers = {
+if __name__ == '__main__':
+    headers = {
     'Accept': 'application/vnd.timetree.v1+json',
     'Authorization': 'Bearer ' + ACCESS_TOKEN
-}
+    }
 
-URL = 'https://timetreeapis.com/calendars/' + CALENDAR_ID + '/upcoming_events?timezone=Asia/Tokyo'
+    url = 'https://timetreeapis.com/calendars/' + CALENDAR_ID + '/upcoming_events?timezone=Asia/Tokyo&days=7'
+    response = requests.get(url, headers=headers)
+    print(response)
+    data = response.json()
+    print(json.dumps(data, indent=4, ensure_ascii=False))
 
-r = requests.get(URL, headers=headers)
-data = r.json()
-
-
-# 予定の表示
-for event in data['data']:
-    print(event['attributes']['title'])
+    for i in range(len(data['data'])):
+        print(data['data'][i]['attributes']['title'])
+        print(data['data'][i]['attributes']['start_at'][:16])
+        print(data['data'][i]['attributes']['end_at'][:16])
