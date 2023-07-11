1542eb85cd4447762c48941e6dcbb4f131d6f340
takayu <134676218+takayu39@users.noreply.github.com>
2023-07-11 12:25:25 +0900

    timetreeAPIの仮完成
    パーソナルトークンを使用したtaskの取得プログラム
    OAuth認証に書き換える

--- application/task.py
+++ application/task.py
@@ -1,28 +1,67 @@
 import requests
+import datetime
 import json
 import mytoken
 
 # 現在はトークンで認証しているが、いずれOAuth認証にする
-ACCESS_TOKEN = mytoken.timetree_token
-CALENDAR_ID = mytoken.timetree_calendar_id
+__ACCESS_TOKEN = mytoken.timetree_token
 
-def get_caleder():
-    return
-
-# ヘッダーの設定
-if __name__ == '__main__':
+def get_calendars():
     headers = {
-    'Accept': 'application/vnd.timetree.v1+json',
-    'Authorization': 'Bearer ' + ACCESS_TOKEN
+        'Accept': 'application/vnd.timetree.v1+json',
+        'Authorization': 'Bearer ' + __ACCESS_TOKEN
     }
 
-    url = 'https://timetreeapis.com/calendars/' + CALENDAR_ID + '/upcoming_events?timezone=Asia/Tokyo&days=7'
+    url = 'https://timetreeapis.com/calendars'
     response = requests.get(url, headers=headers)
-    print(response)
-    data = response.json()
-    print(json.dumps(data, indent=4, ensure_ascii=False))
+    row_data = response.json()
+    # print(json.dumps(data, indent=4, ensure_ascii=False))
+    ret = []
+    for i in range(len(row_data['data'])):
+        ret.append({
+            'id': row_data['data'][i]['id'],
+            'name': row_data['data'][i]['attributes']['name'],
+            'color': row_data['data'][i]['attributes']['color'],
+            'image': row_data['data'][i]['attributes']['image_url']
+        })
 
-    for i in range(len(data['data'])):
-        print(data['data'][i]['attributes']['title'])
-        print(data['data'][i]['attributes']['start_at'][:16])
-        print(data['data'][i]['attributes']['end_at'][:16])
+    print(json.dumps(ret, indent=4, ensure_ascii=False))
+    return ret
+
+def get_tasks():
+    headers = {
+        'Accept': 'application/vnd.timetree.v1+json',
+        'Authorization': 'Bearer ' + __ACCESS_TOKEN
+    }
+
+    calendars = get_calendars()
+    ret = []
+    for calendar in calendars:
+        url = 'https://timetreeapis.com/calendars/' + calendar['id'] + '/upcoming_events?timezone=Asia/Tokyo&days=7'
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        # print(json.dumps(data, indent=4, ensure_ascii=False))
+        
+        hoge = []
+        for i in range(len(data['data'])):
+            hoge.append({
+                'title': data['data'][i]['attributes']['title'],
+                'start_at': data['data'][i]['attributes']['start_at'][:16],
+                'end_at': data['data'][i]['attributes']['end_at'][:16]
+            })
+        
+        ret.append({
+            'name': calendar['name'],
+            'id': calendar['id'],
+            'color': calendar['color'],
+            'image': calendar['image'],
+            'data': hoge
+        })
+
+    # print(json.dumps(ret, indent=4, ensure_ascii=False))
+    return ret
+
+if __name__ == '__main__':
+    output = get_tasks()
+    with open (file='./application/task.json', mode='w', encoding='utf_8') as f:
+        json.dump(obj=output, fp=f, indent=4, ensure_ascii=False)
