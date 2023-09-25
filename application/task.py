6934fa5ca678d1b17fb65d4c5c7fcb913d3ff545
takayu <134676218+takayu39@users.noreply.github.com>
2023-09-25 12:32:34 +0900

    send request with gcp
    gcpを使ってgoogle calendarにリクエスト

--- application/task.py
+++ application/task.py
@@ -1,19 +1,17 @@
-import requests
-import datetime
-import json
 import mytoken
 
 # 現在はトークンで認証しているが、いずれOAuth認証にする
 __ACCESS_TOKEN = mytoken.timetree_token
+__headers = {
+    'Accept': 'application/vnd.timetree.v1+json',
+    'Authorization': 'Bearer ' + __ACCESS_TOKEN
+}
 
 def get_calendars():
-    headers = {
-        'Accept': 'application/vnd.timetree.v1+json',
-        'Authorization': 'Bearer ' + __ACCESS_TOKEN
-    }
-
+    import requests
+    import json
     url = 'https://timetreeapis.com/calendars'
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=__headers)
     row_data = response.json()
     # print(json.dumps(data, indent=4, ensure_ascii=False))
     ret = []
@@ -25,29 +23,29 @@ def get_calendars():
             'image': row_data['data'][i]['attributes']['image_url']
         })
 
-    print(json.dumps(ret, indent=4, ensure_ascii=False))
+    # print(json.dumps(ret, indent=4, ensure_ascii=False))
     return ret
 
 def get_tasks():
-    headers = {
-        'Accept': 'application/vnd.timetree.v1+json',
-        'Authorization': 'Bearer ' + __ACCESS_TOKEN
-    }
-
+    import requests
+    import json
+    import datetime
+    from dateutil.tz import gettz
     calendars = get_calendars()
     ret = []
     for calendar in calendars:
         url = 'https://timetreeapis.com/calendars/' + calendar['id'] + '/upcoming_events?timezone=Asia/Tokyo&days=7'
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=__headers)
         data = response.json()
-        # print(json.dumps(data, indent=4, ensure_ascii=False))
+        print(json.dumps(data, indent=4, ensure_ascii=False))
         
         hoge = []
         for i in range(len(data['data'])):
+            # TODO: start_atの時間がZ時間なので、日本時間に変換する
             hoge.append({
                 'title': data['data'][i]['attributes']['title'],
-                'start_at': data['data'][i]['attributes']['start_at'][:16],
-                'end_at': data['data'][i]['attributes']['end_at'][:16]
+                # 'start_at': datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%fZ'),
+                # 'end_at': datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S.%fZ')
             })
         
         ret.append({
@@ -62,6 +60,7 @@ def get_tasks():
     return ret
 
 if __name__ == '__main__':
+    import json
     output = get_tasks()
     with open (file='./application/task.json', mode='w', encoding='utf_8') as f:
         json.dump(obj=output, fp=f, indent=4, ensure_ascii=False)
