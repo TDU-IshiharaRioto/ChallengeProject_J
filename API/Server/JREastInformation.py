da9a7e872438dcd882e472d64b4f70c8ddf708fc
IshiharaRioto <21aj013@ms.dendai.ac.jp>
2023-10-09 11:53:58 +0900

    2023年10月9日 最適化2

--- API/Server/JREastInformation.py
+++ API/Server/JREastInformation.py
@@ -28,28 +28,3 @@ def getJREastInformation():
             # print(lineList[i].getText() + " " + statusList[i].getText().replace("\n", ""))
 
     return resultList
-
-def getJREastTohokuInformation():
-    url = "https://traininfo.jreast.co.jp/train_info/tohoku.aspx"
-
-    responce = req.get(url)
-    responce.encoding = responce.apparent_encoding
-
-    bs = BeautifulSoup(responce.text, "html.parser")
-
-    lineList = bs.find_all ("span", class_="name")
-    statusList = bs.find_all ("div", class_="rosen_infoBox")
-
-    resultList = []
-
-    for i in range(len(lineList)):
-        resultList.append(lineList[i].getText())
-        resultList.append(statusList[i].getText().replace("\n", ""))
-        # print(lineList[i].getText() + " " + statusList[i].getText().replace("\n", ""))
-    return resultList
-
-def getShinkansenInformation():
-    TokaiUrl = "https://traininfo.jr-central.co.jp/shinkansen/pc/ja/index.html"
-    HokkaidouUrl = "https://www3.jrhokkaido.co.jp/webunkou/senku.html?id=24"
-    WestSanyoUrl = "https://trafficinfo.westjr.co.jp/sanyo.html"
-    WestHokurikuUrl = "https://trafficinfo.westjr.co.jp/h_shinkansen.html"
---
+++ API/Server/JRShinkansen.py
@@ -0,0 +1,5 @@
+def getShinkansenInformation():
+    TokaiUrl = "https://traininfo.jr-central.co.jp/shinkansen/pc/ja/index.html"
+    HokkaidouUrl = "https://www3.jrhokkaido.co.jp/webunkou/senku.html?id=24"
+    WestSanyoUrl = "https://trafficinfo.westjr.co.jp/sanyo.html"
+    WestHokurikuUrl = "https://trafficinfo.westjr.co.jp/h_shinkansen.html"
