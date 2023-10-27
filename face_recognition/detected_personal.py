bba6d5a659117918fd0203ae86bc078a05aecf78
KSTScoreBoard <105205538+NaKaTsu1106@users.noreply.github.com>
2023-10-27 21:33:03 +0900

    Update detected_personal.py

--- face_recognition/detected_personal.py
+++ face_recognition/detected_personal.py
@@ -14,7 +14,6 @@ def signal_handler(sig, frame):
     print("Shutting down gracefully...")
     server.shutdown()
     cv2.destroyAllWindows()
-    cam.release()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -47,6 +46,64 @@ def detect_faces(gray, face_cascade, minW, minH):
         minSize=(int(minW), int(minH)),
     )
 
+def main():
+    recognizer, face_cascade = initialize_recognizer('trainer.yml', 'haarcascade_frontalface_default.xml')
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    names = ['None', 'nakatsu',"obama"]
+
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)
+    cam.set(4, 480)
+
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+    prev_id = None
+    last_detected_time = None
+    
+
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detect_faces(gray, face_cascade, minW, minH)
+        global detected_printed
+
+        if len(faces) > 0:
+            if not detected_printed:
+                message = '{"isDetected" : true}'
+                server.send_message_to_all(message)
+                print("検出")
+                detected_printed = True
+            last_detected_time = time.time()
+        elif last_detected_time and time.time() - last_detected_time > 5 and detected_printed:
+            message = '{"isDetected" : false}'
+            server.send_message_to_all(message)
+            print("非検出")
+            detected_printed = False
+            prev_id = None
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+            
+            id_name = names[id] if confidence < 50 else "unknown"
+            confidence_text = "  {0}%".format(round(100 - confidence))
+            
+            if prev_id == None and id_name != "unknown":
+                print(f"{id_name} を認識")
+                prev_id = id_name
+                
+            cv2.putText(img, str(id_name), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, confidence_text, (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+        #cv2.imshow('camera', img)
+        time.sleep(0.5)
+        if cv2.waitKey(10) & 0xff == 27:
+            break
+
+    print("プログラムを終了します。")
+    cam.release()
+    cv2.destroyAllWindows()
+
 # WebSocketサーバーの初期設定
 server = WebsocketServer(host="127.0.0.1", port=5001)
 server.set_fn_new_client(new_client)
@@ -58,52 +115,4 @@ thread = threading.Thread(target=server.run_forever)
 thread.start()
 
 print("start")
-
-recognizer, face_cascade = initialize_recognizer('trainer.yml', 'haarcascade_frontalface_default.xml')
-font = cv2.FONT_HERSHEY_SIMPLEX
-names = ['None', 'nakatsu',"obama"]
-
-cam = cv2.VideoCapture(0)
-cam.set(3, 640)
-cam.set(4, 480)
-
-minW = 0.1 * cam.get(3)
-minH = 0.1 * cam.get(4)
-prev_id = None
-last_detected_time = None
-
-while True:
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detect_faces(gray, face_cascade, minW, minH)
-
-    if len(faces) > 0:
-        if not detected_printed:
-            message = '{"isDetected" : true}'
-            server.send_message_to_all(message)
-            print("検出")
-            detected_printed = True
-        last_detected_time = time.time()
-    elif last_detected_time and time.time() - last_detected_time > 5 and detected_printed:
-        message = '{"isDetected" : false}'
-        server.send_message_to_all(message)
-        print("非検出")
-        detected_printed = False
-        prev_id = None
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-        
-        id_name = names[id] if confidence < 50 else "unknown"
-        confidence_text = "  {0}%".format(round(100 - confidence))
-        
-        if prev_id == None and id_name != "unknown":
-            print(f"{id_name} を認識")
-            prev_id = id_name
-            
-        cv2.putText(img, str(id_name), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
-        cv2.putText(img, confidence_text, (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
-
-    #cv2.imshow('camera', img)
-    time.sleep(0.05)
+main()
