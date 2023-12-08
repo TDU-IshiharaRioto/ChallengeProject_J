ad5127f3824855240d59a93781a3bc0f739f7622
KST電子工作同好会_LED得点掲示板 <105205538+NaKaTsu1106@users.noreply.github.com>
2023-12-08 12:07:49 +0900

    Update azure_assistant_add.py

--- chat/azure_assistant_add.py
+++ chat/azure_assistant_add.py
@@ -77,18 +77,21 @@ def message_received(client, server, sned_message):
     if data['type'] == 'CONNECT':
         clients[data['name']] = client
 
-    print(data['type'])
-    print(data['name'])
     if data['type'] == 'RESPONSE':
-        if(client == clients['train']):
-            print(sned_message)
+        if(data['name'] == 'train'):
+            messages_history.append({"role": "system", "content": "以下のデータを使って一つ前の質問に答えてください" + str(data['data'])})
+            response = openai.ChatCompletion.create(
+                engine="gpt-35-turbo",
+                messages=messages_history,
+            )
+            s = str(response['choices'][0]['message']['content'])
+            speak_and_dispaly(s)
         if(data['name'] == 'weather'):
             messages_history.append({"role": "system", "content": "以下のデータを使って一つ前の質問に答えてください" + str(data['data'])})
             response = openai.ChatCompletion.create(
                 engine="gpt-35-turbo",
                 messages=messages_history,
             )
-
             speak_and_dispaly(response['choices'][0]['message']['content'])
 
     
